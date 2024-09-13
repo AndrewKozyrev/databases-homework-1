dcf3385b68d88fbb113eede725a05c399823ca2d
AndrewKozyrev <mr.andrey.kozyrev@gmail.com>
2024-09-14 00:07:02 +0300

    added run_sql_scripts.bat changed docker-compose.yml

--- task1/run_sql_scripts.bat
+++ task1/run_sql_scripts.bat
@@ -5,12 +5,12 @@ SET DB_USER=student09
 SET DB_PASSWORD=mF2gT0pO6bV4wJ5m
 
 :: List of SQL files to be executed
-SET SQL_FILES=tb_artists.sql tb_genres.sql tb_albums.sql tb_tracks.sql tb_track_genres.sql
+SET SQL_FILES=tb_artists.sql tb_genres.sql tb_albums.sql tb_tracks.sql tb_track_genres.sql fill_tables.sql
 
 :: Loop through each SQL file and execute it
 FOR %%f IN (%SQL_FILES%) DO (
     echo Executing %%f ...
-    docker exec -i %CONTAINER_NAME% psql -U %DB_USER% -d %DB_NAME% -f ./%%f
+    docker exec -i %CONTAINER_NAME% psql -U %DB_USER% -d %DB_NAME% -f /scripts/%%f
     IF ERRORLEVEL 1 (
         echo Error occurred while executing %%f.
         EXIT /B 1
